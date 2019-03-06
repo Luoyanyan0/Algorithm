2954beaa8f028e941ba2afdcd8eb34aff0253877
Yangxian <847488367@qq.com>
2019-03-06 23:39:10 +0800

    my commit 20190306

--- src/simple/algorithm/SortAndSearch.java
+++ src/simple/algorithm/SortAndSearch.java
@@ -22,6 +22,15 @@ public class SortAndSearch {
         }
     }
 
+    /**
+     * 因为这里最外面的while循环里面还套了两个while循环，也就是说，
+     * 大的while里面的操作会随时改变low和high的值，它们的值也随时会被用到，
+     * 这个时候，如果不在每个while中判断它们的大小的话，就会出现错误。
+     * @param arr
+     * @param low
+     * @param high
+     * @return
+     */
     private int Partition(int[] arr, int low, int high) {
         int pivot = arr[low];
         while(low < high){
@@ -35,4 +44,37 @@ public class SortAndSearch {
         arr[low] = pivot;
         return low;
     }
+
+
+    @Test
+    public void binarySearchTest(){
+        int arr[] = {0,2,3,4,6,9};
+        int target = 2;
+        System.out.println(binarySearch(arr,target));
+    }
+
+    /**
+     * 因为这里while循环里面的条件是if，else，只执行其中之一，虽然对low和high有操作，
+     * 但是操作完成之后是直接返回while循环继续判断的，所以只需要在while里面判断low和high
+     * @param arr
+     * @param target
+     * @return
+     */
+    public int binarySearch(int arr[],int target){
+        int low = 0;
+        int high = arr.length - 1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if (target > arr[mid]){
+                low = mid + 1;
+            }else if(target < arr[mid]){
+                high = mid - 1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+
 }
